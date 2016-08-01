0ac308e2df4900583c89ef3d9d27d6412cdc15f4
George Anastasiou <dr.george995@gmail.com>
2016-08-01 18:56:58 +0300

    Added index page. Created res directory for css and js files.

---
+++ Web Auction Site/WebContent/index.jsp
@@ -0,0 +1,28 @@
+<html>
+  <head>
+    <link rel="stylesheet" type="text/css" href="res/styles.css">
+    <title>Web Auction Site by RobCo Industries</title>
+  </head>
+  <body>
+    <header class="menubar">
+      <a href="">Login</a> |
+      <a href="">Register</a>
+    </header>
+    <div id="search">
+      <h1>Web Auction Site</h1>
+      <form>
+        <div class="txtbtncontainer">
+          <input type="text" placeholder="Search..." class="textbox-search" name="search_terms" />
+          <input type="submit" class="button-search" value="Search"/>
+          <select name="category" class="select-search">
+            <option value="all">All Categories</option>
+            <option value="tech">Technology</option>
+            <option value="clothes">Clothing</option>
+          </select>
+        </div>
+      </form>
+    </div>
+    <footer>RobCo Industries 2016</footer>
+  </body>
+</html>
+  
---
+++ Web Auction Site/src/servlets/IndexServlet.java
@@ -0,0 +1,41 @@
+package servlets;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class IndexServlet
+ */
+@WebServlet("")
+public class IndexServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public IndexServlet() {
+        super();
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		RequestDispatcher disp = getServletContext().getRequestDispatcher("/index.jsp");
+		disp.forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
