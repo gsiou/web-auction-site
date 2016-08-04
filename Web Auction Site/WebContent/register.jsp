7eed0553ffe2e08cfbe1f5e029e3b57dfd384646
George Anastasiou <dr.george995@gmail.com>
2016-08-04 18:53:58 +0300

    Added basic registration support.

--- Web Auction Site/WebContent/register.jsp
+++ Web Auction Site/WebContent/register.jsp
@@ -1,3 +1,5 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
 <html>
 	<head>
 	<link rel="stylesheet" type="text/css" href="res/styles.css">
@@ -7,16 +9,23 @@
 <body>
 	<header class="logo" > Hammer Deals<br>Register </header>
 	<div class="reg-table">
-		<input type="text" name="Username" class="textbox-register" placeholder="Username"><br>
-		<input type="password" name="Password" class="textbox-register" placeholder="Password"><br>
-		<input type="password" name="Password_conf" class="textbox-register" placeholder="Confirm Password"><br>
-		<input type="email" name="Email" class="textbox-register" placeholder="Email"><br>
-		<input type="number" name="Trn" class="textbox-register" placeholder="Trn"><br>
-		<input type="number" name="Phone" class="textbox-register" placeholder="Phone"><br>
-		<input type="text" name="Address" class="textbox-register" placeholder="Address"><br>
-		<input type="text" name="Country" class="textbox-register" placeholder="Country"><br>
-		
-		<input type="submit" class="button-register" value="Register">
+		<form action="Registration" method="post">
+			<h3 style="color: red">${message}</h3>
+			<input type="text" name="Username" class="textbox-register"
+				placeholder="Username"><br> <input type="password"
+				name="Password" class="textbox-register" placeholder="Password"><br>
+			<input type="password" name="Password_conf" class="textbox-register"
+				placeholder="Confirm Password"><br> <input type="email"
+				name="Email" class="textbox-register" placeholder="Email"><br>
+			<input type="number" name="Trn" class="textbox-register"
+				placeholder="Trn"><br> <input type="number"
+				name="Phone" class="textbox-register" placeholder="Phone"><br>
+			<input type="text" name="Address" class="textbox-register"
+				placeholder="Address"><br> <input type="text"
+				name="Country" class="textbox-register" placeholder="Country"><br>
+
+			<input type="submit" class="button-register" value="Register">
+		</form>
 	</div>
 </body>
 </html>
---
+++ Web Auction Site/src/servlets/UserRegistrationServlet.java
@@ -0,0 +1,134 @@
+package servlets;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import dao.UserDAO;
+import dao.UserDAOI;
+import entities.User;
+
+/**
+ * Servlet implementation class UserRegistrationServlet
+ */
+@WebServlet("/Registration")
+public class UserRegistrationServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public UserRegistrationServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		//doPost(request, response);
+		RequestDispatcher disp = getServletContext().getRequestDispatcher("/register.jsp");
+		disp.forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		boolean register_success = false;
+		String msg = "";
+		
+		// Gather form data.
+		String userid = request.getParameter("Username");
+		String password1 = request.getParameter("Password");
+		String password2 = request.getParameter("Password_conf");
+		String email = request.getParameter("Email");
+		String address = request.getParameter("Address");
+		String country = request.getParameter("Country");
+		String phone = request.getParameter("Phone");
+		String trn = request.getParameter("Trn");
+		// Validate form input.
+		if(userid == null){
+			msg = "Username cannot be empty.";
+		}
+		else if(password1 == null){
+			msg = "Password cannot be empty.";
+		}
+		else if(password2 == null){
+			msg = "You have to verify your password.";
+		}
+		else if(email == null){
+			msg = "Email cannot be empty.";
+		}
+		else if(address == null){
+			msg = "Address cannot be empty.";
+		}
+		else if(country == null){
+			msg = "Country cannot be empty.";
+		}
+		else if(phone == null){
+			msg = "Phone cannot be empty.";
+		}
+		else if(trn == null){
+			msg = "Tax Registration Number cannot be empty.";
+		}
+		else{
+			// We have what we need.
+			
+			if(password1.length() == 0){
+				// We cannot accept empty passwords.
+				msg = "You cannot use an empty password.";
+			}
+			else if(!password1.equals(password2)){
+				// Passwords must match.
+				msg = "Passwords do not match";
+			}
+			else{
+				// Create user object.
+				User user = new User();
+				user.setUserId(userid);
+				user.setAccess_lvl(0); // Default access level.
+				user.setPassword(password1); // TODO: hash this
+				user.setBid_rating(0);
+				user.setSell_rating(0);
+				user.setLatitude(0); // TODO: add support for this
+				user.setLongitude(0); // TODO: add support for this
+				user.setCountry(country);
+				user.setAddress(address);
+				user.setPhone(Integer.parseInt(phone.trim()));
+				user.setEmail(email);
+				user.setTrn(Integer.parseInt(trn.trim()));
+				
+				// Create dao object to insert user to db.
+				UserDAOI dao = new UserDAO();
+				if(dao.create(user)){
+					// Success.
+					msg = "User registration succeded.";
+					register_success = true;
+				}
+				else{
+					// Duplicate username.
+					msg = "User with that username exists.";
+				}
+			}
+		}
+		RequestDispatcher disp;
+		request.setAttribute("message", msg);
+		if(register_success){
+			// Redirect to success page
+			disp = getServletContext().getRequestDispatcher("/reg_success.jsp");
+		}
+		else{
+			// Redirect back to registration page with message.
+			disp = getServletContext().getRequestDispatcher("/register.jsp");
+		}
+		disp.forward(request, response);
+	}
+
+}
