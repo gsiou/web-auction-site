e7e2c0f2d4775e0c881c1c1cfb46f16e3a243275
George Anastasiou <dr.george995@gmail.com>
2016-09-28 15:56:37 +0300

    Forgot to include file in previous commit (I suck)

---
+++ Web Auction Site/WebContent/res/search_fetch_categories.js
@@ -0,0 +1,70 @@
+$(function() {
+	var send_data = {
+		parent_category : "",
+	};
+	$.ajax({
+		url : 'AuctionSubmit?action=fetch_categories',
+		type : "POST",
+		data : JSON.stringify(send_data),
+		contentType : "application/json; charset=utf-8",
+		dataType : 'json',
+		success : function(data) {
+			$.each(data.categories, function(i, item) {
+				$("#category-dropdown").append($("<option/>", {
+					value : item,
+					text : item,
+				}));
+			});
+		},
+	});
+	
+	$("#refresh_btn").click(function(){
+		window.categories_last = "0";
+		fetch("");
+	});
+});
+
+
+window.categories_text = "";
+window.categories_last = "0";
+
+function fetch(parent){
+	var send_data={
+			parent_category : parent,
+	};
+	$("#category_list").hide(100);
+	if(window.categories_last != parent){
+		$("#categories_tbox").val(parent);
+		window.categories_last = parent;
+		$("#category_list").html("");
+		$.ajax({
+			url : 'AuctionSubmit?action=fetch_categories',
+			type: "POST",
+			data : JSON.stringify(send_data),
+			contentType: "application/json; charset=utf-8",
+			dataType: 'json',
+			success:function(data){
+				$.each(data.categories, function(i, item){
+					$("<span/>", {
+						html: $("<button/>", {
+							id: item,
+							text: item + " ",
+							onclick: "fetch('" + item + "')",
+							class: "button-category",
+							type: "button"
+						})
+					}).appendTo("#category_list");
+				});
+			},
+			error: function(){
+				alert("Connection to database failed! Try refreshing the page.");
+			}
+		});
+		$(document).scrollTop();
+	}
+	else{
+		$("#category_list").html("");
+		$("#category_btn").show(1000);
+	}
+	$("#category_list").show(1000);
+}
